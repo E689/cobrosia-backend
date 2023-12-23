35b0c5746911e89719a3ff6ef09b3f25e9899af6
tiagosansopa <tiagosansopa@gmail.com>
2023-12-23 03:35:03 -0600

    clients routes

--- controllers/clients.js
+++ controllers/clients.js
@@ -1,28 +1,73 @@
 const Clients = require("../models/clients");
 
 exports.createClient = (req, res) => {
-  const { name, userId } = req.body;
-  if (!name || !userId) {
+  const { clientName, userId, contactName, contactlastName, phone, email } =
+    req.body;
+  if (
+    !clientName ||
+    !userId ||
+    !contactName ||
+    !contactlastName ||
+    !phone ||
+    !email
+  ) {
     return res.status(400).json({
-      message: "Missing parameters. Please enter name and userId",
+      message:
+        "Missing parameters. Please enter clientName, userId, contactName, contactlastName, phone, email",
     });
   }
 
-  const newClient = new Clients({ name, user: userId });
+  const newClient = new Clients({ name: clientName, user: userId });
 
   newClient
     .save()
     .then((newClient) => {
-      return res.status(201).json({
-        message: "Client created",
-        client: { name: newClient.name, id: newClient._id },
+      const newContact = new Contacs({
+        name: contactName,
+        lastName: contactlastName,
+        phone,
+        email,
+        client: newClient._id,
       });
+
+      newContact
+        .save()
+        .then((newContact) => {
+          return res.status(201).json({
+            message: "Client created",
+            client: { name: newContact.name, id: newContact._id },
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+          return res.status(500).json({
+            error,
+            message: "Error creating contact",
+          });
+        });
     })
     .catch((error) => {
       console.log(error);
       return res.status(500).json({
         error,
         message: "Error creating client",
+      });
+    });
+};
+
+exports.getClientsByUser = (req, res) => {
+  const id = req.params.id;
+  Clients.find({ user: id })
+    .then((clients) => {
+      return res.status(200).json({
+        clients,
+        message: "Clients from user retrieved",
+      });
+    })
+    .catch((error) => {
+      return res.status(500).json({
+        error,
+        message: "Error finding Client",
       });
     });
 };

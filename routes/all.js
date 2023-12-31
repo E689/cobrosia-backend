ddab7638c25016dc1713660fc6de825e7f96d18d
tiagosansopa <tiagosansopa@gmail.com>
2023-12-31 04:04:35 -0600

    model import

--- routes/all.js
+++ routes/all.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const OpenAI = require("openai");
+const Clients = require("../models/clients");
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_KEY,
 });
